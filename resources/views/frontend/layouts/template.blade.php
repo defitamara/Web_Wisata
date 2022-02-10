b2046a0d25757cdadd4d1847851ac96439e5d817
Defi Tamara <defytamara2610@gmail.com>
2022-02-10 10:53:00 +0700

    Mengganti tampilan frontend

--- resources/views/frontend/layouts/template.blade.php
+++ resources/views/frontend/layouts/template.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>BizPage Bootstrap Template</title>
+  <title>{{ config('app.name') }}</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -14,7 +14,7 @@
   <link href="{{ asset('frontend/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="{{ asset('frontend/assets/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
@@ -28,8 +28,8 @@
   <link href="{{ asset('frontend/assets/css/style.css') }}" rel="stylesheet">
 
   <!-- =======================================================
-  * Template Name: BizPage - v5.8.0
-  * Template URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+  * Template Name: eBusiness - v4.7.0
+  * Template URL: https://bootstrapmade.com/ebusiness-bootstrap-corporate-template/
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
@@ -43,18 +43,14 @@
 
   @include('frontend/layouts.footer')
 
+  <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-  <!-- Uncomment below i you want to use a preloader -->
-  <!-- <div id="preloader"></div> -->
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('frontend/assets/vendor/purecounter/purecounter.js') }}"></script>
-  <script src="{{ asset('frontend/assets/vendor/aos/aos.js') }}"></script>
   <script src="{{ asset('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('frontend/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
   <script src="{{ asset('frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
   <script src="{{ asset('frontend/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('frontend/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
   <script src="{{ asset('frontend/assets/vendor/php-email-form/validate.js') }}"></script>
 
   <!-- Template Main JS File -->
